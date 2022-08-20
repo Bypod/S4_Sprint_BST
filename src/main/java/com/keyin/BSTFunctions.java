467ddee50f4501fee00307e577a33ff53eb288ce
John Byrne <94805286+Bypod@users.noreply.github.com>
2022-08-20 00:51:38 -0230

    Update BSTFunctions.java

--- src/main/java/com/keyin/BSTFunctions.java
+++ src/main/java/com/keyin/BSTFunctions.java
@@ -6,20 +6,28 @@ public class BSTFunctions {
 
     public static void main (String[] args){
 
-        Scanner input = new Scanner(System.in);
-        int[] inputs = new int[8];
+//         for (int i = 0; i < inputs.length; i++){
+//         System.out.println("\nPlease enter numbers up to 10, Type 'create' to start creation of tree: ");
+//             System.out.println("\nPlease enter 8 numbers");
+//             inputs[i] = input.nextInt();
+//         }
 
+       
 
-        for (int i = 0; i < inputs.length; i++){
-//          System.out.println("\nPlease enter numbers up to 10, Type 'create' to start creation of tree: ");
-            System.out.println("\nPlease enter 8 numbers");
-            inputs[i] = input.nextInt();
-        }
+        // for (int in : inputs){
 
-        BST newTree =
+        // }
 
-        for (int in: inputs){
+         BST newTree = new BST();
 
-        }
+         newTree.insert(10)
+         newTree.insert(30)
+         newTree.insert(15)
+         newTree.insert(90)
+         newTree.insert(30)
+         newTree.insert(12)
+
+         newTree.inOrder(newTree.getRoot())
+
     }
 }
